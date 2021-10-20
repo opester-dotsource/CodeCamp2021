5fbdc64e52b74637b633c5552c7e63ce79c2e1c9
Oskar <oskar.pester@gmail.com>
2021-10-20 12:50:50 +0200

    inti

---
+++ public/index.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      blogPosts: [
+          {
+              "title":"First blogpost",
+              "description":"Short description text. Displayed in List.",
+              "content":"Content text displayed when clicked on Blogpost"
+          },
+          {
+              "title":"Second blogpost",
+              "description":"Short description text. Displayed in List.",
+              "content":"Content text displayed when clicked on Blogpost"
+          },
+          {
+              "title":"Third blogpost",
+              "description":"Short description text. Displayed in List.",
+              "content":"Content text displayed when clicked on Blogpost"
+          },
+          {
+              "title":"Fourth blogpost",
+              "description":"Short description text. Displayed in List.",
+              "content":"Content text displayed when clicked on Blogpost"
+          }
+      ]
+    }
+  })
